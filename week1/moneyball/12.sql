8c63aec7eb91904ef54b4b6ddf36354ae70485ff
books1021 <lawwaihung20@gmail.com>
2026-04-13 21:14:33 +0900

    moneyball with CTE method

--- week1/moneyball/12.sql
+++ week1/moneyball/12.sql
@@ -38,6 +38,63 @@ FROM (
     )
 ORDER BY "last_name" ASC
 ;
+
+-- CTE method
+WITH "year_2001" AS (
+    SELECT
+        "p"."id",
+        "p"."first_name",
+        "p"."last_name",
+        "s"."salary",
+        "perf"."H",
+        "perf"."RBI"
+    FROM "players" AS "p"
+    JOIN "salaries" AS "s"
+      ON "p"."id" = "s"."player_id"
+    JOIN "performances" AS "perf"
+      ON "p"."id" = "perf"."player_id"
+     AND "perf"."year" = "s"."year"
+    WHERE "s"."year" = 2001
+),
+"top_hit" AS (
+    SELECT "id"
+    FROM "year_2001"
+    WHERE "H" <> 0
+    ORDER BY
+        "salary" * 1.0 / "H" ASC,
+        "first_name" ASC,
+        "last_name" ASC,
+        "id" ASC
+    LIMIT 10
+),
+"top_rbi" AS (
+    SELECT "id"
+    FROM "year_2001"
+    WHERE "RBI" <> 0
+    ORDER BY
+        "salary" * 1.0 / "RBI" ASC,
+        "first_name" ASC,
+        "last_name" ASC,
+        "id" ASC
+    LIMIT 10
+),
+"both_top10" AS (
+    SELECT "id" FROM "top_hit"
+    INTERSECT
+    SELECT "id" FROM "top_rbi"
+)
+SELECT
+    "p"."first_name",
+    "p"."last_name"
+FROM "players" AS "p"
+JOIN "both_top10" AS "b"
+  ON "p"."id" = "b"."id"
+ORDER BY
+    "p"."last_name" ASC,
+    "p"."first_name" ASC,
+    "p"."id" ASC;
+
+
 --  query value of best 10th hit and best 10th RBI
 -- then use it to query names
 SELECT "first_name",
