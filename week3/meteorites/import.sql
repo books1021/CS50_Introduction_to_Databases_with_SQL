8909907d44138d81ff0e6aba927f4e7b34834e52
books1021 <lawwaihung20@gmail.com>
2026-04-27 18:59:49 +0900

    meteorites round up

--- week3/meteorites/import.sql
+++ week3/meteorites/import.sql
@@ -1,16 +1,21 @@
 -- sql command to import csv to db, run inside sqlite
 -- .import --csv meteorites.csv meteorites_temp
+--reset codes
+DROP TABLE IF EXISTS "meteorites_temp";
+DROP TABLE IF EXISTS "meteorites";
 
 -- create temp table and final table, with constrain.
 CREATE TABLE "meteorites_temp" (
-    "id" INTEGER,
     "name" TEXT DEFAULT NULL,
+    "id" INTEGER,
+    "nametype" TEXT DEFAULT NULL,
     "class" TEXT DEFAULT NULL,
     "mass" REAL DEFAULT NULL,
     "discovery" TEXT DEFAULT NULL,
     "year" REAL DEFAULT NULL,
     "lat" REAL DEFAULT NULL,
     "long" REAL DEFAULT NULL,
+    PRIMARY KEY ('id')
 );
 
 CREATE TABLE "meteorites" (
@@ -22,6 +27,35 @@ CREATE TABLE "meteorites" (
     "year" REAL DEFAULT NULL,
     "lat" REAL DEFAULT NULL,
     "long" REAL DEFAULT NULL,
+    PRIMARY KEY ('id')
 );
 
-.import --csv meteorites.csv meteorites_temp
+.import --csv --skip 1 meteorites.csv meteorites_temp
+
+--empty value to NULL
+UPDATE "meteorites_temp" 
+SET "mass" = NULL WHERE "mass" = '';
+
+UPDATE "meteorites_temp"
+    SET "year" = NULL
+    WHERE "year" = '';
+
+UPDATE "meteorites_temp"
+    SET "lat" = NULL
+    WHERE "lat" = '';
+
+UPDATE "meteorites_temp"
+    SET "long" = NULL
+    WHERE "long" = '';
+
+-- round to 0.01
+UPDATE "meteorites_temp" 
+SET "mass" = ROUND("mass",2);
+
+UPDATE "meteorites_temp"
+    SET "lat" = NULL
+    WHERE "lat" = ROUND("lat",2);
+
+UPDATE "meteorites_temp"
+    SET "long" = NULL
+    WHERE "long" = ROUND("long",2);
